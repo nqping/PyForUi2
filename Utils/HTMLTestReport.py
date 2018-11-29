436b9f6511c81cc6dd900259a27c8a8e5c562a87
qingping.niu <mobile#3>
2018-11-29 13:56:29 +0800

    自动化测试平台

--- Utils/HTMLTestReport.py
+++ Utils/HTMLTestReport.py
@@ -173,10 +173,10 @@ class Template_mixin(object):
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <link href = "http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel = "stylesheet">
-    <script src = "http://libs.baidu.com/jquery/2.0.0/jquery.min.js"> </ script>
-    <script src = "http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js" > </script>
-    <script src = "https://img.hcharts.cn/highcharts/highcharts.js"> </script>
-    <script src = "https://img.hcharts.cn/highcharts/modules/exporting.js"> </script>
+    <script src = "http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+    <script src = "http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src = "https://img.hcharts.cn/highcharts/highcharts.js"></script>
+    <script src = "https://img.hcharts.cn/highcharts/modules/exporting.js"></script>
 
 
 
