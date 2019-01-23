781aebc2e0f003c0d125a3f0f1393394b5816cc4
qingping.niu <mobile#3>
2019-01-23 15:45:01 +0800

    加入jenkins 接收ip参数

--- run_cases.py
+++ run_cases.py
@@ -5,7 +5,7 @@
 # @File    : run_cases.py
 # @desc    :
 
-import sys
+import sys,argparse
 sys.path.append('.')
 from Utils.CaseStrategy import CaseStrategy
 from Utils.Drivers import Drivers
@@ -13,10 +13,18 @@ from Utils.Drivers import Drivers
 if __name__ == '__main__':
     # back up old report dir 备份旧的测试报告文件夹到TestReport_backup下
     # backup_report()
+    #获取jenkins传IP参数
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", required=True, help="ip")
+    args = vars(ap.parse_args())
+    test_ip = args['ip']
+    print(test_ip)
 
     cs = CaseStrategy()
     cases = cs.collect_cases(suite=True)
-    Drivers().run(cases)
+    Drivers().run(cases,test_ip)
 
     # Generate zip_report file  压缩测试报告文件
     # zip_report()
+
+
