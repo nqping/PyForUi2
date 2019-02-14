2b6d081c3ede9d1abaf93655da94f8ba629deaa9
qingping.niu <mobile#3>
2019-02-14 16:55:37 +0800

    os.popen

--- Public/adbCommand.py
+++ Public/adbCommand.py
@@ -5,17 +5,16 @@
 # @File    : adbCommand.py
 # @desc    :
 
-import subprocess
+import subprocess,os
 
 def get_pid(devices,grepname):
     '''获取进程PID'''
     cmd = 'adb -s %s shell ps |findstr %s' % (devices,grepname)
-    output = subprocess.getstatusoutput(cmd)
-    if output[0] == 1:
-        pid = None
-    else:
-        pid = output[1].split()[1]
-    # print('%s pid=%s'%(grepname,pid))
+    output = os.popen(cmd).read()
+    pid = None
+    if output != "":
+        pid = output.split()[1]
+
     return pid
 
 def kill_process(devices,pid):
@@ -24,3 +23,7 @@ def kill_process(devices,pid):
     subprocess.getoutput(killcmd)
 
 
+
+if __name__=='__main__':
+    pid = get_pid('a96cd66d', 'logcat')
+    print(pid)
