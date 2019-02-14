e017b5e0d6f507328009d90e57037c3ff9cecefc
qingping.niu <mobile#3>
2019-02-14 17:02:01 +0800

    环境命令

--- Public/adbCommand.py
+++ Public/adbCommand.py
@@ -5,11 +5,21 @@
 # @File    : adbCommand.py
 # @desc    :
 
-import subprocess,os
+import subprocess,os,platform
+
+def getSystemStatus():#获取系统的名称，使用对应的指令
+    system=platform.system()
+    if system=='Windows':
+        find_manage='findstr'
+    else:
+        find_manage='grep'
+    return  find_manage
+
+findstr = getSystemStatus()
 
 def get_pid(devices,grepname):
     '''获取进程PID'''
-    cmd = 'adb -s %s shell ps |findstr %s' % (devices,grepname)
+    cmd = 'adb -s %s shell ps | %s %s' % (devices,findstr,grepname)
     output = os.popen(cmd).read()
     pid = None
     if output != "":
@@ -21,7 +31,6 @@ def kill_process(devices,pid):
     #获取android系统版本
     killcmd = 'adb -s %s shell kill %s'%(devices,pid)
     subprocess.getoutput(killcmd)
-
 
 
 if __name__=='__main__':
